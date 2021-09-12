e87e1558cd0958dfc7437272ba0688841e3d76e3
BLooDBRothER <computertechnology446@gmail.com>
2021-09-12 17:30:31 +0530

    Add backspace funcitonality

--- main.js
+++ main.js
@@ -23,8 +23,13 @@ let valid = {
     ")": ")",
     ".": "."
 }
-window.addEventListener("keypress", (e) => {
+
+window.addEventListener("keydown", (e) => {
+    if(e.key === "Backspace"){
+        expressionCnt.innerText = expressionCnt.innerText.slice(0, expressionCnt.innerText.length-1);
+    }
     if((e.key>="0" && e.key<="9") || valid[e.key]){
+        addKeyPressEffects(e.key);
         expressionCnt.innerText += e.key;
         expressionCnt.scrollTop = expressionCnt.scrollHeight;
     }
