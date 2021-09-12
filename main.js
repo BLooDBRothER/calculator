ebf12bae40ef0ebe97c685024e4898a9dd98d92a
BLooDBRothER <computertechnology446@gmail.com>
2021-09-12 17:05:07 +0530

    Add listener

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
 const buttons = document.querySelectorAll(".button");
+const expressionCnt = document.querySelector(".expression");
 
 buttons.forEach(button => {
     button.addEventListener("click", (e) => {
-        console.log(e.target.innerText);
+        if(e.target.innerText === "A/C"){
+            expressionCnt.innerText = '';
+            return;
+        };
+        expressionCnt.innerText += e.target.innerText;
+        expressionCnt.scrollTop = expressionCnt.scrollHeight;
     });
+});
+
+let valid = {
+    "=": "=",
+    "+": "+",
+    "-": "-",
+    "*": "*",
+    "/": "/",
+    "^": "^",
+    "(": "(",
+    ")": ")",
+    ".": "."
+}
+window.addEventListener("keypress", (e) => {
+    if((e.key>="0" && e.key<="9") || valid[e.key]){
+        expressionCnt.innerText += e.key;
+        expressionCnt.scrollTop = expressionCnt.scrollHeight;
+    }
 });
