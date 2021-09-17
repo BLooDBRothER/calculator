f2ea0ad9c4988af361371f3b36c7813ac5e59434
BLooDBRothER <computertechnology446@gmail.com>
2021-09-17 14:49:08 +0530

    Add evaluation logic

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import { stack } from "./Module/stack.js";
+
 const buttons = document.querySelectorAll(".button");
 const expressionCnt = document.querySelector(".expression");
 
@@ -12,9 +14,85 @@ const regex = {
     operators: /[+\-*\/\^]{2,}/gi
 }
 
+function evaluateTwoNumbers(no1, no2, op){
+    switch(op){
+        case '+': return no1 + no2;
+        case '-': return no1 - no2;
+        case '*': return no1 * no2;
+        case '/': return no1 / no2;
+    }
+}
+
+function isNumber(val){
+    return ((val >= "0" && val <= "9") || val === '.') ? true : false;
+}
+
+function operatorPrecedence(op){
+    if(op === '+' || op === '-'){
+        return 1;
+    }
+    if(op === '*' || op === '/'){
+        return 2;
+    }
+    if(op === '^'){
+        return 3;
+    }
+    return -1;
+}
+
+function evaluateExpression(expression){
+    let i;
+    const op = new stack(),
+    no = new stack();
+    for(i=0; i<expression.length; i++){
+        if(expression[i] === '('){
+            op.push(expression[i]);
+        }
+        else if(expression[i] === ')'){
+            while(!op.isEmpty() && op.top() !== '('){
+                let no2 = no.pop();
+                let no1 = no.pop();
+                let ope = op.pop();
+                no.push(evaluateTwoNumbers(no1, no2, ope));
+            }
+            op.pop();
+        }
+        else if(isNumber(expression[i])){
+            let val='';
+            while(i<expression.length && isNumber(expression[i])){
+                val += expression[i];
+                i++;
+            }
+            no.push(parseFloat(val));
+            i--;
+        }
+        else{
+            while(!op.isEmpty() && operatorPrecedence(op.top()) >= operatorPrecedence(expression[i])){
+                let no2 = no.pop();
+                let no1 = no.pop();
+                let ope = op.pop();
+                no.push(evaluateTwoNumbers(no1, no2, ope));
+            }
+            op.push(expression[i]);
+        }
+    }
+    while(!op.isEmpty()){
+        let no2 = no.pop();
+        let no1 = no.pop();
+        let ope = op.pop();
+        no.push(evaluateTwoNumbers(no1, no2, ope));
+    }
+    console.log(no.top());
+}
+
 buttons.forEach(button => {
     button.addEventListener("click", (e) => {
         
+        if(e.target.innerText === "="){
+            evaluateExpression(expressionCnt.innerText);
+            return;
+        }
+
         if(e.target.innerText === "A/C"){
             expressionCnt.innerText = '';
             return;
@@ -58,7 +136,8 @@ let valid = {
     "/": buttons[17],
     ".": buttons[18],
     "0": buttons[19],
-    "=": buttons[20]
+    "=": buttons[20],
+    "Enter": buttons[20]
 }
 
 function addActiveElement(elem){
