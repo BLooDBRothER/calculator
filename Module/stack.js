e4fe16981a2782a91178d0cabc6ac3ffae3743fc
BLooDBRothER <computertechnology446@gmail.com>
2021-09-17 11:59:17 +0530

    stack implementation

---
+++ Module/stack.js
@@ -0,0 +1,20 @@
+export class stack{
+    constructor(){
+        this.data = [];
+        this.size = 0;
+    }
+    push(value){
+        this.data[this.size++] = value;
+    }
+    pop(){
+        if(this.isEmpty()) return ; 
+        let popValue = this.data[--this.size];
+        return popValue;
+    }
+    top(){
+        return this.data[this.size-1];
+    }
+    isEmpty(){
+        return this.top() ? false : true;
+    }
+}
